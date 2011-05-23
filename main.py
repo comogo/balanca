95fbdeeba1edd4e2c20b91bdac5bb4a4a05dabd6
comogo <mcomogo@gmail.com>
2011-05-23 14:11:21 -0300

    Refatorando.. Classe

--- main.py
+++ main.py
@@ -1,58 +1,127 @@
+#!/usr/bin/env python
 # *-* coding: utf-8 -*-
 import ConfigParser
 import serial
 import time
-import re
 import os
 
-CONFIG_DEFAULT = 'config.cfg'
 
-def ler_configs(filename=CONFIG_DEFAULT):
+class Balanca(object):
     """
-    Lê as configurações do arquivo.
+    Classe que representa a balança.
     """
-    config = ConfigParser.ConfigParser()
-    config.read(filename)
-    conf = {
-        'port':     config.get('connection', 'port', '0'),
-        'baudrate': int(config.get('connection', 'baudrate', '9600')),
-        'parity':   config.get('connection', 'parity', 'N'),
-        'bytesize': int(config.get('connection', 'bytesize', '8')),
-        'stopbits': int(config.get('connection', 'stopbits', '1')),
-        'timeout': float(config.get('connection', 'timeout', '1')),
-    }
-    saida = config.get('out', 'file', '/home/balanca/peso.txt')
-    return (conf, saida,)
+    
+    CONFIG_FILENAME = 'config.cfg'
+    
+    def __init__(self, port=0, baudrate=9600, parity='N', bytesize=8, stopbits=1, timeout=0, output_filename='/home/balanca/peso.txt'):
+        self.port = port
+        self.baudrate = baudrate
+        self.parity = parity
+        self.bytesize = bytesize
+        self.stopbits = stopbits
+        self.timeout = timeout
+        self.output_filename = output_filename
+        
+        self.connection = None
+        self.previous_data  = ''
+        self.actual_data = ''
+    
+    def load_config(self, filename=None):
+        """
+        Lê as configurações do arquivo.
+        """
+        if filename is None:
+            filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), self.CONFIG_FILENAME)
+        
+        config = ConfigParser.ConfigParser()
+        config.read(filename)
+        
+        self.port = config.get('connection', 'port', '0')
+        self.baudrate = int(config.get('connection', 'baudrate', '9600'))
+        self.parity   = config.get('connection', 'parity', 'N')
+        self.bytesize = int(config.get('connection', 'bytesize', '8'))
+        self;stopbits = int(config.get('connection', 'stopbits', '1'))
+        self.timeout  = float(config.get('connection', 'timeout', '1'))
+        self.output_filename = config.get('out', 'file', self.output_filename)
 
-def gravar_valor(filename, valor):
-    f = open(filename, 'w')
-    f.write(valor)
-    f.close()
+    def __write_data(self, data):
+        """
+        Escreve os dados no arquivo de saída.
+        """
+        f = open(self.output_filename, 'w')
+        f.write(data)
+        f.close()
+
+    def __read_raw_data(self):
+        """
+        Lê dados da balança.
+        """
+        return self.connection.read(500)
+    
+    def __open_connection(self):
+        """
+        Abre uma conexão com a porta serial.
+        """
+        ok = False
+        while not ok:
+            try:
+                self.connection = serial.Serial(port=self.port, baudrate=self.baudrate,
+                                stopbits=self.stopbits, parity=self.parity, 
+                                bytesize=self.bytesize, timeout=self.timeout)
+                ok = True
+            except:
+                self.connection.close()
+
+    def __close_connection(self):
+        """
+        Fecha a conexão com a porta serial.
+        """
+        self.connection.close()
+    
+    def __reset_connection(self):
+        self.__close_connection()
+        self.__open_connection()
+    
+    def __get_data(self, data):
+        """
+        Transforma a entrada raw em valor.
+        Caso não haja dados retorna None.
+        """
+        from re import findall
+        
+        matches = findall(' \d+ ', leitura)
+        if len(matches) > 0:
+            return matches[-1].strip()
+        return None
+        
+                                
+    def run_forever(self, sleep_time=0.2):
+        """
+        Função princioal
+        """
+        self.__open_connection()                   
+        while True:
+            try:
+                raw_data = self.__read_raw_data()
+                data = self.__get_data(raw_data)
+                if data:
+                    self.actual_data = data
+                    if self.actual_data != self.previous_data:
+                        self.__write_data(self.actual_data)
+                        self.previous_data = self.actual_data
+                else:
+                    self.__write_data('')
+                    self.actual_data = ''
+                    self.previous_data = ''
+            except Exception,e:
+                self.__write_data('')
+                self.actual_data = ''
+                self.previous_data = ''
+                self.__reset_connection()
+                
+            time.sleep(sleep_time)
 
 if __name__=="__main__":
-    filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), CONFIG_DEFAULT)
-    config, filename = ler_configs(filename)
-    valor_anterior = ''
-    valor_atual = ''
-    while True:
-        try:
-            ser = serial.Serial(**config)
-            leitura = ser.read(500)
-            matches = re.findall(' \d+ ', leitura)
-            if len(matches) > 0:
-                valor_atual = matches[-1].strip()
-                if valor_atual != valor_anterior:
-                    gravar_valor(filename, valor_atual)
-                    valor_anterior = valor_atual
-            else:
-                gravar_valor(filename, '')
-                valor_anterior = ''
-                valor_atual = ''
-                
-        except Exception,e:
-            gravar_valor(filename, '')
-            valor_anterior = ''
-            valor_atual = ''
-            ser.close()
-            ser = serial.Serial(**config)
-        time.sleep(0.2)
+    balanca = Balanca()
+    balanca.load_config()
+    balanca.run_forever()
