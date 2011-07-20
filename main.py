d7c28ea29f7a5848b01d2c5c05ad2ea0d5aeaef0
comogo <mcomogo@gmail.com>
2011-07-20 17:23:57 -0300

    Add tipos de balança e modo debug no arquivo de configuração

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ class Balanca(object):
     """
     Classe que representa a balança.
     """
-    
+
     CONFIG_FILENAME = 'config.cfg'
-    
+
     def __init__(self, port=0, baudrate=9600, parity='N', bytesize=8, stopbits=1, timeout=0, output_filename='/home/balanca/peso.txt'):
         self.port = port
         self.baudrate = baudrate
@@ -21,21 +21,25 @@ class Balanca(object):
         self.stopbits = stopbits
         self.timeout = timeout
         self.output_filename = output_filename
-        
+
+        self.regex = ''
+        self.debug = False
+        self.tipo_balanca = 'b1'
+
         self.connection = None
         self.previous_data  = ''
         self.actual_data = ''
-    
+
     def load_config(self, filename=None):
         """
         Lê as configurações do arquivo.
         """
         if filename is None:
             filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), self.CONFIG_FILENAME)
-        
+
         config = ConfigParser.ConfigParser()
         config.read(filename)
-        
+
         self.port = config.get('connection', 'port', '0')
         self.baudrate = int(config.get('connection', 'baudrate', '9600'))
         self.parity   = config.get('connection', 'parity', 'N')
@@ -43,21 +47,30 @@ class Balanca(object):
         self;stopbits = int(config.get('connection', 'stopbits', '1'))
         self.timeout  = float(config.get('connection', 'timeout', '1'))
         self.output_filename = config.get('out', 'file', self.output_filename)
+        self.debug = config.get('misc', 'debug', '0') != '0'
+        self.tipo_balanca = config.get('misc', 'tipo_balanca', 'b1')
+        self.regex = config.get('regex', self.tipo_balanca, '" \d+ "')[1:-1]
 
     def __write_data(self, data):
         """
         Escreve os dados no arquivo de saída.
         """
+        data.strip()
         f = open(self.output_filename, 'w')
         f.write(data)
         f.close()
+        if self.debug:
+            print "Escrevendo >> ", data
 
     def __read_raw_data(self):
         """
         Lê dados da balança.
         """
-        return self.connection.read(500)
-    
+        read = self.connection.read(500)
+        if self.debug:
+            print "Lendo      >> ", read
+        return read
+
     def __open_connection(self):
         """
         Abre uma conexão com a porta serial.
@@ -66,7 +79,7 @@ class Balanca(object):
         while not ok:
             try:
                 self.connection = serial.Serial(port=self.port, baudrate=self.baudrate,
-                                stopbits=self.stopbits, parity=self.parity, 
+                                stopbits=self.stopbits, parity=self.parity,
                                 bytesize=self.bytesize, timeout=self.timeout)
                 ok = True
             except:
@@ -77,29 +90,29 @@ class Balanca(object):
         Fecha a conexão com a porta serial.
         """
         self.connection.close()
-    
+
     def __reset_connection(self):
         self.__close_connection()
         self.__open_connection()
-    
+
     def __get_data(self, data):
         """
         Transforma a entrada raw em valor.
         Caso não haja dados retorna None.
         """
         from re import findall
-        
-        matches = findall(' \d+ ', leitura)
+
+        matches = findall(self.regex, data)
         if len(matches) > 0:
             return matches[-1].strip()
         return None
-        
-                                
+
+
     def run_forever(self, sleep_time=0.2):
         """
         Função princioal
         """
-        self.__open_connection()                   
+        self.__open_connection()
         while True:
             try:
                 raw_data = self.__read_raw_data()
@@ -118,10 +131,11 @@ class Balanca(object):
                 self.actual_data = ''
                 self.previous_data = ''
                 self.__reset_connection()
-                
+
             time.sleep(sleep_time)
 
 if __name__=="__main__":
     balanca = Balanca()
     balanca.load_config()
     balanca.run_forever()
+
